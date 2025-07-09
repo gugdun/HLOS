cf85e01a72d166bd7e54af8faf00da8a47f8ba82
gugdun <gugdunoff@gmail.com>
2025-07-10 02:46:04 +0300

    fix: Virtual memory mapping

--- kernel/memory/paging.c
+++ kernel/memory/paging.c
@@ -8,8 +8,12 @@
 #define HEAP_MIN_SIZE 512       // In 4 KiB pages
 
 __attribute__((aligned(PAGE_SIZE_4KB))) static uint64_t pml4[512];
-__attribute__((aligned(PAGE_SIZE_4KB))) static uint64_t pdpt[512];
-__attribute__((aligned(PAGE_SIZE_4KB))) static uint64_t pds[PAGING_MAP_GIB][512]; // one PD per GiB
+
+__attribute__((aligned(PAGE_SIZE_4KB))) static uint64_t pdpt_low[512];
+__attribute__((aligned(PAGE_SIZE_4KB))) static uint64_t pds_low[PAGING_MAP_GIB][512];  // one PD per GiB
+
+__attribute__((aligned(PAGE_SIZE_4KB))) static uint64_t pdpt_high[512];
+__attribute__((aligned(PAGE_SIZE_4KB))) static uint64_t pds_high[PAGING_MAP_GIB][512]; // one PD per GiB
 
 uint64_t next_virtual_heap_addr = VIRT_HEAP_BASE;
 
@@ -23,8 +27,8 @@ static void map_identity(struct MemoryMapEntry *entry)
         uint64_t pdpt_index = (addr >> 30) & 0x1FF;
         uint64_t pd_index   = (addr >> 21) & 0x1FF;
 
-        pds[pdpt_index][pd_index] = addr | PAGE_PRESENT | PAGE_RW | PAGE_PS;
-        pdpt[pdpt_index] = ((uint64_t)pds[pdpt_index]) | PAGE_PRESENT | PAGE_RW;
+        pds_low[pdpt_index][pd_index] = addr | PAGE_PRESENT | PAGE_RW | PAGE_PS;
+        pdpt_low[pdpt_index] = ((uint64_t)pds_low[pdpt_index]) | PAGE_PRESENT | PAGE_RW;
     }
 
 #ifdef HLOS_DEBUG
@@ -44,19 +48,11 @@ static void map_virtual(struct MemoryMapEntry *entry)
     for (size_t offset = 0; offset < aligned_size; offset += PAGE_SIZE_2MB) {
         uint64_t phys = phys_start + offset;
         uint64_t virt = virt_start + offset;
-        uint64_t pml4_index = (virt >> 39) & 0x1FF;
         uint64_t pdpt_index = (virt >> 30) & 0x1FF;
         uint64_t pd_index   = (virt >> 21) & 0x1FF;
 
-        if (!(pml4[pml4_index] & PAGE_PRESENT)) {
-            pml4[pml4_index] = (uint64_t)&pdpt[pml4_index] | PAGE_PRESENT | PAGE_RW;
-        }
-
-        if (!(pdpt[pml4_index] & PAGE_PRESENT)) {
-            pdpt[pml4_index] = (uint64_t)&pds[pml4_index][pdpt_index] | PAGE_PRESENT | PAGE_RW;
-        }
-
-        pds[pml4_index][pd_index] = phys | PAGE_PRESENT | PAGE_RW | PAGE_PS;
+        pds_high[pdpt_index][pd_index] = phys | PAGE_PRESENT | PAGE_RW | PAGE_PS;
+        pdpt_high[pdpt_index] = ((uint64_t)pds_high[pdpt_index]) | PAGE_PRESENT | PAGE_RW;
     }
 
 #ifdef HLOS_DEBUG
@@ -68,12 +64,12 @@ static void map_virtual(struct MemoryMapEntry *entry)
 
 void setup_paging(struct MemoryMapEntry *memory_map, size_t memory_map_size, size_t descriptor_size, uint64_t fb_base, size_t fb_size)
 {
-    // Identity map framebuffer
+    // Map framebuffer
     size_t fb_pages = (fb_size / (uint64_t)PAGE_SIZE_4KB) + 1;
     struct MemoryMapEntry fb_entry = { 0, 0, fb_base, fb_base, (uint64_t)fb_pages, 0 };
     map_identity(&fb_entry);
 
-    // Identity map entries
+    // Map UEFI memory map entries
     for (size_t i = 0; i < memory_map_size; i += descriptor_size) {
         struct MemoryMapEntry *entry = (struct MemoryMapEntry *)((uint8_t *)memory_map + i);
         switch (entry->type) {
@@ -92,18 +88,23 @@ void setup_paging(struct MemoryMapEntry *memory_map, size_t memory_map_size, siz
             case UnacceptedMemory:
                 map_identity(entry);
                 break;
+
             case ConventionalMemory:
                 if (entry->size_pages >= HEAP_MIN_SIZE) {
                     map_virtual(entry);
                 }
                 break;
+
             default:
                 break;
         }
     }
 
-    // Identity map PDPT into PML4[0] (used for low memory)
-    pml4[0] = ((uint64_t)pdpt) | PAGE_PRESENT | PAGE_RW;
+    // Lower-half identity mapped
+    pml4[0] = ((uint64_t)pdpt_low) | PAGE_PRESENT | PAGE_RW;
+
+    // Higher-half contains conventional memory regions
+    pml4[256] = ((uint64_t)pdpt_high) | PAGE_PRESENT | PAGE_RW;
 
     // Enable paging
     uint64_t pml4_phys = (uint64_t)pml4;
