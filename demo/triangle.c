2f3e3a29774f2f30ed53465754a107a9f1ba1963
gugdun <gugdunoff@gmail.com>
2025-07-12 03:39:31 +0300

    refactor: Incapsulate triangle demo

---
+++ demo/triangle.c
@@ -0,0 +1,94 @@
+#include <demo/triangle.h>
+
+#include <kernel/io/tty.h>
+#include <kernel/timer/sleep.h>
+#include <kernel/memory/paging.h>
+#include <kernel/memory/bitmap.h>
+
+struct DemoTriangleState demo_triangle_init(void)
+{
+    // Enable double-buffering
+    if (fb_get_size() > 0) {
+        bool fail = false;
+        void *fb_buffer = alloc_page();
+        if (fb_buffer == NULL) fail = true;
+        
+        size_t fb_pages = fb_get_size() / PAGE_SIZE_2MB;
+        for (size_t i = 0; i < fb_pages; ++i) {
+            void *page = alloc_page();
+            if (page == NULL) {
+                fail = true;
+                break;
+            }
+        }
+
+        if (!fail) {
+            fb_init_buffer(fb_buffer);
+        } else {
+            tty_printf("[Kernel] Failed to allocate memory for double-buffering.\n");
+        }
+    }
+
+    // Create state
+    struct DemoTriangleState state = {
+        0.0f,
+        fb_color_rgb(0.1f, 0.1f, 0.1f),
+        fb_color_rgb(0.1f, 0.7f, 0.2f),
+        {  0.0f,  0.5f },
+        { -0.5f, -0.5f },
+        {  0.5f, -0.5f }
+    };
+
+    return state;
+}
+
+void demo_triangle_tick(struct DemoTriangleState *state)
+{
+    fb_clear(state->clear_color);
+
+    // Rotate vertices
+    const float angle = state->angle;
+    matrix2x2 rotation_matrix = {
+        .m = {
+            {cos(angle), -sin(angle)},
+            {sin(angle), cos(angle)}
+        }
+    };
+
+    vector2 rotated_v1 = matrix2x2_mul_vector2(rotation_matrix, state->v1);
+    vector2 rotated_v2 = matrix2x2_mul_vector2(rotation_matrix, state->v2);
+    vector2 rotated_v3 = matrix2x2_mul_vector2(rotation_matrix, state->v3);
+
+    // Correct aspect ratio
+    const float width = fb_get_width();
+    const float height = fb_get_height();
+    const float aspect = (float)width / (float)height;
+    rotated_v1.x /= aspect;
+    rotated_v2.x /= aspect;
+    rotated_v3.x /= aspect;
+
+    rotated_v1.x = (rotated_v1.x + 1.0f) * (width / 2.0f);
+    rotated_v1.y = (rotated_v1.y + 1.0f) * (height / 2.0f);
+    rotated_v2.x = (rotated_v2.x + 1.0f) * (width / 2.0f);
+    rotated_v2.y = (rotated_v2.y + 1.0f) * (height / 2.0f);
+    rotated_v3.x = (rotated_v3.x + 1.0f) * (width / 2.0f);
+    rotated_v3.y = (rotated_v3.y + 1.0f) * (height / 2.0f);
+
+    // Render
+    fb_triangle_fill(
+        state->triangle_color,
+        rotated_v1.x, rotated_v1.y,
+        rotated_v2.x, rotated_v2.y,
+        rotated_v3.x, rotated_v3.y
+    );
+
+    fb_present();
+    ksleep(16);
+
+    // Update state
+    float new_angle = angle + 0.016f;
+    if (new_angle > 2.0f * M_PI) {
+        new_angle -= 2.0f * M_PI;
+    }
+    state->angle = new_angle;
+}
---
+++ include/demo/triangle.h
@@ -0,0 +1,19 @@
+#ifndef _DEMO_TRIANGLE_H
+#define _DEMO_TRIANGLE_H
+
+#include <lib/math.h>
+#include <kernel/graphics/framebuffer.h>
+
+struct DemoTriangleState {
+    float angle;
+    fb_color_t clear_color;
+    fb_color_t triangle_color;
+    vector2 v1;
+    vector2 v2;
+    vector2 v3;
+};
+
+struct DemoTriangleState demo_triangle_init(void);
+void demo_triangle_tick(struct DemoTriangleState *state);
+
+#endif
