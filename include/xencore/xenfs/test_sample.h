44b9cdb935b8a140626ab8c3c85c2183b1248b38
gugdun <gugdunoff@gmail.com>
2025-07-15 03:47:05 +0300

    add: Test sample parsing

--- include/xencore/xenfs/test_sample.h
+++ include/xencore/xenfs/test_sample.h
@@ -3,6 +3,36 @@
 
 #include <stdint.h>
 
+#define TAR_BLOCK_SIZE       512
+#define TAR_REGULAR_FILE     '0'
+#define TAR_HARD_LINK        '1'
+#define TAR_SYMBOLIC_LINK    '2'
+#define TAR_CHARACTER_DEVICE '3'
+#define TAR_BLOCK_DEVICE     '4'
+#define TAR_DIRECTORY        '5'
+#define TAR_FIFO             '6'
+#define TAR_CONTIGUOUS_FILE  '7'
+
+struct TarHeader {
+    char name[100];
+    char mode[8];
+    char uid[8];
+    char gid[8];
+    char size[12];
+    char mtime[12];
+    char chksum[8];
+    char typeflag;
+    char linkname[100];
+    char magic[6];
+    char version[2];
+    char uname[32];
+    char gname[32];
+    char devmajor[8];
+    char devminor[8];
+    char prefix[155];
+    char pad[12];
+};
+
 struct TestSampleParams {
     uint64_t addr;
     uint64_t size;
--- xencore/xenfs/test_sample.c
+++ xencore/xenfs/test_sample.c
@@ -4,6 +4,59 @@
 static uint8_t *sample_base = NULL;
 static size_t sample_size = 0;
 
+static inline uint64_t oct2dec(const char *str, size_t size) {
+    uint64_t result = 0;
+    for (size_t i = 0; i < size && str[i]; ++i) {
+        result = (result << 3) + (str[i] - '0'); // Multiply by 8
+    }
+    return result;
+}
+
+static void parse_tar(const void *tar_start, size_t tar_size) {
+    const uint8_t *ptr = (const uint8_t *)tar_start;
+
+    while ((size_t)ptr < (size_t)tar_start + tar_size - TAR_BLOCK_SIZE * 2) {
+        struct TarHeader *hdr = (struct TarHeader *)ptr;
+
+        // End of archive = two empty blocks
+        if (hdr->name[0] == '\0') break;
+
+        const uint64_t file_size = oct2dec(hdr->size, sizeof(hdr->size));
+        const void *file_data = ptr + TAR_BLOCK_SIZE;
+
+        char *type_name = "unknown";
+        switch (hdr->typeflag) {
+            case '0': type_name = "regular file"; break;
+            case '1': type_name = "hard link"; break;
+            case '2': type_name = "symbolic link"; break;
+            case '3': type_name = "character device"; break;
+            case '4': type_name = "block device"; break;
+            case '5': type_name = "directory"; break;
+            case '6': type_name = "FIFO"; break;
+            case '7': type_name = "contiguous file"; break;
+            default: break;
+        }
+
+        if (file_size > tar_size - (ptr - (const uint8_t *)tar_start) - TAR_BLOCK_SIZE) {
+            tty_printf("[Test Sample] Error: File size exceeds remaining TAR size\n");
+            break;
+        }
+
+        tty_printf(
+            "[Test Sample] Found %s: %s (%u bytes)\n",
+            type_name,
+            hdr->name,
+            file_size
+        );
+
+        // TODO: add file to VFS
+
+        // Advance pointer: header + file data (rounded up to 512)
+        size_t file_blocks = (file_size + TAR_BLOCK_SIZE - 1) / TAR_BLOCK_SIZE;
+        ptr += TAR_BLOCK_SIZE + file_blocks * TAR_BLOCK_SIZE;
+    }
+}
+
 void analyse_test_sample(struct TestSampleParams *params)
 {
     sample_base = (uint8_t *)params->addr;
@@ -12,8 +65,5 @@ void analyse_test_sample(struct TestSampleParams *params)
         "[Test Sample] %u bytes @ 0x%x\n",
         sample_size, (uint64_t)sample_base
     );
-    tty_printf(
-        "         | 0x00 0x01 0x02 0x03\n  -------+--------------------\n  0x0000 | 0x%x 0x%x 0x%x 0x%x\n",
-        sample_base[0], sample_base[1], sample_base[2], sample_base[3]
-    );
+    parse_tar(sample_base, sample_size);
 }
